96487cc28078431b07eda7fd214759e710413569
Yago Silva <yago.silva@vivareal.com>
2016-07-09 01:33:30 -0300

    Adiciona um sleep entre as intermediate e a terminal operation na classe de exemplo 'LazyEvaluation'

--- src/main/java/br/com/thedevelopersconference/streams/demo/LazyEvaluation.java
+++ src/main/java/br/com/thedevelopersconference/streams/demo/LazyEvaluation.java
@@ -6,11 +6,21 @@ import java.util.stream.Stream;
 public class LazyEvaluation {
 
     public static void main(String[] args) {
-        Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).map(i -> {
+        Stream<Integer> stream = Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).map(i -> {
             System.out.println(i+"Passou por aqui");
             return i;
-        }).collect(Collectors.toList());
+        });
         
+        try {
+            Thread.sleep(1000);
+            System.out.println("Que loucura");
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        
+        stream.collect(Collectors.toList());
         System.out.println("fim");
     }
 }
