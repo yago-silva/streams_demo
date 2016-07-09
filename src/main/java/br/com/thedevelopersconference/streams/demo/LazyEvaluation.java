425006b3ce650fd9500965a5b2d758e5db33565d
Yago Silva <yago.silva@vivareal.com>
2016-07-09 01:31:27 -0300

    Adiciona class de exemplo 'LazyEvaluation' sem nenhuma terminal operation

---
+++ src/main/java/br/com/thedevelopersconference/streams/demo/LazyEvaluation.java
@@ -0,0 +1,15 @@
+package br.com.thedevelopersconference.streams.demo;
+
+import java.util.stream.Stream;
+
+public class LazyEvaluation {
+
+    public static void main(String[] args) {
+        Stream<Integer> stream = Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).map(i -> {
+            System.out.println(i+"Passou por aqui");
+            return i;
+        });
+        
+        System.out.println("fim");
+    }
+}
