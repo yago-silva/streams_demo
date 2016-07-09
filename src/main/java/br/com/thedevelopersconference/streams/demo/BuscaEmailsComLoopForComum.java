61632d162da63985bfee5cecb64758d8d5170ed9
Yago Silva <yago.silva@vivareal.com>
2016-07-08 21:16:10 -0300

    Adiciona implementação para busca de email com loop for

--- src/main/java/br/com/thedevelopersconference/streams/demo/BuscaEmailsComLoopForComum.java
+++ src/main/java/br/com/thedevelopersconference/streams/demo/BuscaEmailsComLoopForComum.java
@@ -8,5 +8,16 @@ public class BuscaEmailsComLoopForComum {
     public static void main(String[] args) {
         // Objetivo: Email de 2 participantes inscritos na RedHat e que ganharam uma camisa P.
         List<Participante> participantesTdc = ParticipantesTdc.listaTodos();
+        
+        List<String> emails = new ArrayList<>();
+        
+        for(int i = 0 ; i < participantesTdc.size() && emails.size() < 2; i++){
+            Participante participante = participantesTdc.get(i);
+            if(participante.isInscrito() && participante.ganhouCamisaP()){
+                emails.add(participante.getEmail());
+            }
+        }
+        
+        System.out.println(emails);
     }
 }
